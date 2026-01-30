a30309c3f53e11e62216a1facede37e8a0e05d00
Dmitry Soboliev <19leva89@gmail.com>
2026-01-30 15:15:21 +0200

    upd dependencies

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,37 +1,19 @@
 import 'dotenv/config'
-import { PrismaPg } from '@prisma/adapter-pg'
 
-import { getDatabaseUrl } from './prisma-cert'
+import pg from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 //! Do not change the path, made for seed.ts
 import { PrismaClient } from '../generated/prisma/client'
 
 const isProduction = process.env.NODE_ENV === 'production'
+const connectionString = `${process.env.DATABASE_URL}`
 
-// Get the database connection string with SSL parameters
-const databaseUrl = getDatabaseUrl()
+const pool = new pg.Pool({ connectionString })
 
-const adapter = new PrismaPg({
-	connectionString: databaseUrl,
-	// connectionParams: {
-	// 	ssl: {
-	// 		rejectUnauthorized: true,
-	// 		ca: process.env.CA_CERT,
-	// 	},
-	// },
+const adapter = new PrismaPg(pool)
+
+export const prisma = new PrismaClient({
+	adapter,
+	log: isProduction ? ['warn', 'error'] : ['info', 'warn', 'error'],
 })
-
-const globalForPrisma = global as unknown as {
-	prisma: PrismaClient
-}
-
-const prisma =
-	globalForPrisma.prisma ||
-	new PrismaClient({
-		adapter,
-		log: isProduction ? ['warn', 'error'] : ['info', 'warn', 'error'],
-	})
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-export { prisma }
